a5064961298802b542beea24239a31a99ebe4630
KM127PL <km127pl2@gmail.com>
2021-10-30 23:29:02 +0200

    Add comments

--- src/main.c
+++ src/main.c
@@ -9,17 +9,20 @@ int main() {
     int running = 1;
     char answer;
 
+    // Main game loop
     while (running) {
-        printf("Guess a number between 1 and 100: \n> ");
+        printf("Guess a number between 1 and 100: \n> "); // Prompt user for input.
         scanf("%d", &guess);
         tries++;
 
+        // Check if the guess is correct
         if (guess == randomNumber) {
             printf("You guessed it! It was %d, it took you %d try/tries.\n", guess, tries);
 
             printf("Do you want to play again? (1/0) \n> ");
             scanf("%d", &answer);
 
+            // Check if the user wants to play again
             if (answer) {
                 tries = 0;
                 randomNumber = rand() % 100 + 1;
